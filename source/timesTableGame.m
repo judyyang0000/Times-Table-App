ca29c3daa135b993fb8e85293707b050d920cabc
Judy <Judy@LAPTOP-A6T69SRE.mshome.net>
2022-04-25 13:17:04 +0800

    modified by Judy 20220425

--- source/timesTableGame.m
+++ source/timesTableGame.m
@@ -4,7 +4,7 @@ classdef timesTableGame < handle
     % Copyright 2018-2020 The MathWorks, Inc.
     
     properties
-        CurrentTimesTable = 3;
+        CurrentTimesTable = 4;
         CurrentQuestion;
         NumberCorrectAnswers = 0;
     end
